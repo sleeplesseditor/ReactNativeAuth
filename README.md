74ef7801210a322c4dd865641afad645609fa45f
sleeplesseditor <josh@bryson.me.uk>
2018-04-26 12:03:02 +0100

    App finalised

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # ReactNativeAuth  
 
-This is a simple demonstration of the use of authorization with React Native. The app uses Firebase for a database role.
+This is a simple demonstration of the use of authorization with React Native. The app uses Firebase for an authentication role with the 'email/password option.
+
+It also uses common components from my earlier [albums project](https://github.com/sleeplesseditor/Albums).
+
+![screen shot 2018-04-26 at 11 54 21 2](https://user-images.githubusercontent.com/25869284/39302171-9db9430c-4949-11e8-821d-4669a26c7bcd.png)
+
+App landing screen
+
+
+![screen shot 2018-04-26 at 11 55 27 2](https://user-images.githubusercontent.com/25869284/39302184-ad810216-4949-11e8-9864-1ea82b531a5d.png)
+
+App loading after user email and password entered
+
+
+![screen shot 2018-04-26 at 11 54 46 2](https://user-images.githubusercontent.com/25869284/39302201-bc4d8f6c-4949-11e8-90cc-f0a9830e284a.png)
+
+Authentication failure message
+
+
+![screen shot 2018-04-26 at 11 56 08 6](https://user-images.githubusercontent.com/25869284/39302212-c71fe962-4949-11e8-8bf7-d7f0a43cb276.png)
+
+Logout screen
--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Button, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyBI_FZed__A7yUj5iHMsg_tWrTygVcZrvA',
@@ -14,14 +16,39 @@ class App extends Component {
             projectId: 'auth-react-native-abed4',
             storageBucket: 'auth-react-native-abed4.appspot.com',
             messagingSenderId: '573415320284'
-        });   
+        });
+        
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <View style={{ flexDirection: 'row' }}>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </View>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />;
+        }
     }
 
     render() {
         return (
             <View>
-                <Header headerText="Authentication"/>
-                <LoginForm />
+                <Header headerText="Authentication" />
+                {this.renderContent()}
             </View>
         );
     }
